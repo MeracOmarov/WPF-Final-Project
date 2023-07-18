3ffbcbd6f406811c23a79a275d46596fb9f4a0d1
Asadov Ibrahim Perviz <Asado_ho45@itstep.edu.az>
2023-07-18 14:29:35 +0400

    commit

--- Hospital2/ViewModel/ClassGiris.cs
+++ Hospital2/ViewModel/ClassGiris.cs
@@ -7,19 +7,28 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Controls;
+using Hospital.Views;
 
 namespace HOSBITAL.ViewModel
 {
     public class ClassGiris
     {
         public RealCommand? daxilOlCommand { get; set; }
+        public RealCommand? hekimlerCommand { get; set; }
         private void DaxilOl(object? sender)
         {
             Girish.girish!.MainFrame.Content = new Login();
-        } 
+        }
+
+        private void Hekimler(object? sender)
+        {
+            Hekimsexsikobinet hekimsexsikobinet = new Hekimsexsikobinet();
+            hekimsexsikobinet.ShowDialog();
+        }
         public ClassGiris()
         {
             daxilOlCommand = new RealCommand(DaxilOl);
+            hekimlerCommand = new RealCommand(Hekimler);
         }
     }
 }
