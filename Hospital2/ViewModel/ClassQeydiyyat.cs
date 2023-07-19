36fda8455ad714ce9d248d5bd1de0bd17aa932fe
Asadov Ibrahim Perviz <Asado_ho45@itstep.edu.az>
2023-07-19 20:22:04 +0400

    commit

--- Hospital2/ViewModel/ClassQeydiyyat.cs
+++ Hospital2/ViewModel/ClassQeydiyyat.cs
@@ -2,11 +2,59 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Text.RegularExpressions;
 using System.Threading.Tasks;
+using System.Xml.Linq;
+using System.Windows;
+using System.ComponentModel;
 
 namespace HOSBITAL.ViewModel
 {
-    public class ClassQeydiyyat
+    public class ClassQeydiyyat:INotifyPropertyChanged
     {
-    }
+		private string ?_Name;
+
+		public string Name
+		{
+			get { return _Name!; }
+			set {
+                if (value!.Length < 3 || !Regex.Match(value!, @"\b[A-Z][a-z]+\b").Success)
+                {
+                    MessageBox.Show("Invalid Name!", "Error", MessageBoxButton.OK, MessageBoxImage.Error);
+                }
+                _Name = value;
+                OnPropertyChanged(nameof(Name));
+            }
+		}
+        private string? _Surname;
+        public string Surname
+        {
+            get { return _Surname!; }
+            set
+            {
+                if (!Regex.Match(value!, @"\b[A-Z][a-z]+\b").Success)
+                {
+                    MessageBox.Show("Invalid Surname!", "Error", MessageBoxButton.OK, MessageBoxImage.Error);
+                }
+                _Surname = value;
+                OnPropertyChanged(nameof(Surname));
+            }
+        }
+
+        private int myVar;
+
+		public event PropertyChangedEventHandler? PropertyChanged;
+        protected virtual void OnPropertyChanged(string propertyName)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+
+        public int MyProperty
+		{
+			get { return myVar; }
+			set { myVar = value; }
+		}
+
+
+	}
 }
--- Hospital2/ViewModel/PasientClass.cs
+++ Hospital2/ViewModel/PasientClass.cs
@@ -1,4 +1,5 @@
 ﻿using HOSBITAL.Commands;
+using HOSBITAL.Pages;
 using Hospital.Pages;
 using Hospital.Views;
 using System;
@@ -11,16 +12,34 @@ namespace Hospital.ViewModel
 {
     public class PasientClass
     {
+        public RealCommand? qebulCommand { get; set; }
         public RealCommand? onlineNovbeCommand { get; set; }
+        public RealCommand? sualCavabCommand { get; set; }
+        public RealCommand? laboratoriyaCommand { get; set; }
 
+        private void Qebul(object? sender)
+        {
+            Pasientsexsikobinet.pasientsexsikobinet!.MainFrame.Content = new Qebul();
+        }
         private void OnlineNovbe(object? sender)
         {
             Pasientsexsikobinet.pasientsexsikobinet!.MainFrame.Content = new Online_novbe();
         }
 
+        private void SualCavab(object? sender)
+        {
+            Pasientsexsikobinet.pasientsexsikobinet!.MainFrame.Content = new SualCavab();
+        }
+        private void Laboratoriya(object? sender)
+        {
+            Pasientsexsikobinet.pasientsexsikobinet!.MainFrame.Content = new Laboratoriya();
+        }
         public PasientClass()
         {
+            qebulCommand = new(Qebul);
             onlineNovbeCommand = new(OnlineNovbe);
+            sualCavabCommand = new(SualCavab);
+            laboratoriyaCommand = new(Laboratoriya);
         }
     }
 }
