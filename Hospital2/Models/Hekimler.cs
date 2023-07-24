92a266cfb479c651f7a62ad46c686b312a603bc5
Asadov Ibrahim Perviz <Asado_ho45@itstep.edu.az>
2023-07-24 21:43:24 +0400

    commit

--- Hospital2/Models/Hekimler.cs
+++ Hospital2/Models/Hekimler.cs
@@ -12,8 +12,8 @@ namespace Hospital2.Models
     public class Hekimler : INotifyPropertyChanged
     {
 
+        public List<ClassMuracietler>? muracietler { get; set; }
         public List<PasientlerDB> ?pasients { get; set; }
-        public List<ClassMuracietler>? Muracietler { get; set; }
 
 
 
--- Hospital2/ViewModel/ClassTedbirler.cs
+++ Hospital2/ViewModel/ClassTedbirler.cs
@@ -4,9 +4,9 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace HOSBITAL.ViewModel
+namespace Hospital2.ViewModel
 {
-    public class ClassMuracietler
+    internal class ClassTedbirler
     {
     }
 }
